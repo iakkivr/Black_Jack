23d7562dbbe0ad6956787a2ced6be5a5cf5297e9
iakkivr <smolskyi.vitalii@gmail.com>
2022-04-09 23:35:14 +0300

    refactor method calculate

--- player.rb
+++ player.rb
@@ -11,9 +11,6 @@ class User
   end
 
   def score
-    print @cards.map(&:points)
-    puts ''
-    @cards.count
     calculate_value_with_ace
   end
 
@@ -36,13 +33,21 @@ class User
   def calculate_value
     result = []
     @cards.map { |card| result << card.cost unless card.ace? }
-    result
+    result.flatten.sum
   end
 
   def calculate_value_with_ace
-    result = ([1,11] * count_ace).combination(count_ace).to_a.map(&:sum).uniq
-                                 .concat(calculate_value).flatten.filter(&:positive?)
-     result.combination(@cards.count).to_a.map(&:sum)
+    result = []
+    @cards
+    points_without_aces = calculate_value
+   ([1,11] * count_ace).combination(count_ace).to_a.map(&:sum).uniq.each do |elem|
+     result << elem + points_without_aces
+   end
+    result.sort.each do |elem|
+      return result if elem > 21
+      result = elem
+    end
+  #  не комбиинровать , а считать по очереди
   end
 
 end
